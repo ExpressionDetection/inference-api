ab927489158014d7fa8064452a6654009cc2c2ff
root <root@vagrant.vm>
2021-07-30 09:49:12 +0000

    Working sample pusher API using encryption

--- src/api.go
+++ src/api.go
@@ -1,26 +1,218 @@
 package main
-
+    
 import (
 	"os"
-	"github.com/pusher/pusher-http-go"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+	"strconv" 
+	pusher "github.com/pusher/pusher-http-go"
 )
 
-func main(){
+func main() {
 
-	pusherClient := pusher.Client{
-		AppID: os.Getenv("PUSHER_APP_ID"),
-		Key: os.Getenv("PUSHER_KEY"),
-		Secret: os.Getenv("PUSHER_SECRET"),
-		Cluster: os.Getenv("PUSHER_CLUSTER"),
-		Secure: os.Getenv("PUSHER_SECURE") == "true",
+	apiPort, _ := strconv.Atoi(os.Getenv("API_PORT"))
+	port := flag.Int("http.port",  apiPort, "Port to run HTTP service on")
+
+	flag.Parse()
+
+	appID := os.Getenv("PUSHER_APP_ID")
+	appKey := os.Getenv("PUSHER_APP_KEY")
+	appSecret := os.Getenv("PUSHER_APP_SECRET")
+	appCluster := os.Getenv("PUSHER_APP_CLUSTER")
+	appIsSecure := os.Getenv("PUSHER_APP_SECURE")
+
+	var isSecure bool
+	if appIsSecure == "1" {
+		isSecure = true
 	}
 
-	data := map[string]string{"message": "hello world"}
-	pusherClient.Trigger("my-channel", "my-event", data)
+	client := &pusher.Client{
+			AppID:               appID,
+			Key:                 appKey,
+			Secret:              appSecret,
+			Cluster:             appCluster,
+			Secure:              isSecure,
+			EncryptionMasterKey: os.Getenv("PUSHER_CHANNELS_ENCRYPTION_KEY"),
+	}
 
+	mux := http.NewServeMux()
 
+	f := &feed{
+		mu:   &sync.RWMutex{},
+		data: make(map[string]string, 0),
+	}
+
+	mux.Handle("/feed", createFeedTitle(client, f))
+	mux.Handle("/pusher/auth", authenticateUsers(client))
 	
-	for {
-		
-	}
+	log.Println("Starting HTTP server")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+}
+
+type feed struct {
+	data map[string]string
+
+	mu *sync.RWMutex
+}
+
+func (f *feed) exists(title string) bool {
+		f.mu.RLock()
+		defer f.mu.RUnlock()
+		_, ok := f.data[title]
+		return ok
+}
+
+func (f *feed) Add(title, content string) error {
+		if f.exists(title) {
+				return errors.New("title already exists")
+		}
+
+		f.mu.Lock()
+		defer f.mu.Unlock()
+		f.data[title] = content
+		return nil
+}
+
+const (
+		successMsg = "success"
+		errorMsg   = "error"
+)
+
+func createFeedTitle(client *pusher.Client, f *feed) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+				if r.Method == http.MethodOptions {
+						return
+				}
+
+				writer := json.NewEncoder(w)
+
+				type respose struct {
+						Message   string `json:"message"`
+						Status    string `json:"status"`
+						Timestamp int64  `json:"timestamp"`
+				}
+
+				if r.Method != http.MethodPost {
+						w.WriteHeader(http.StatusMethodNotAllowed)
+						writer.Encode(&respose{
+								Message:   http.StatusText(http.StatusMethodNotAllowed),
+								Status:    errorMsg,
+								Timestamp: time.Now().Unix(),
+						})
+
+						return
+				}
+
+				var request struct {
+						Title   string `json:"title"`
+						Content string `json:"content"`
+				}
+
+				if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+						w.WriteHeader(http.StatusBadRequest)
+						writer.Encode(&respose{
+								Message:   "Invalid request body",
+								Status:    errorMsg,
+								Timestamp: time.Now().Unix(),
+						})
+						return
+				}
+
+				if len(strings.TrimSpace(request.Title)) == 0 {
+						w.WriteHeader(http.StatusBadRequest)
+						writer.Encode(&respose{
+								Message:   "Title field is empty",
+								Status:    errorMsg,
+								Timestamp: time.Now().Unix(),
+						})
+						return
+				}
+
+				if len(strings.TrimSpace(request.Content)) == 0 {
+						w.WriteHeader(http.StatusBadRequest)
+						writer.Encode(&respose{
+								Message:   "Content field is empty",
+								Status:    errorMsg,
+								Timestamp: time.Now().Unix(),
+						})
+						return
+				}
+
+				if err := f.Add(request.Title, request.Content); err != nil {
+						w.WriteHeader(http.StatusAlreadyReported)
+						writer.Encode(&respose{
+								Message:   err.Error(),
+								Status:    errorMsg,
+								Timestamp: time.Now().Unix(),
+						})
+						return
+				}
+
+				go func() {
+
+						err := client.Trigger("private-encrypted-feeds", "items", map[string]string{
+								"title":     request.Title,
+								"content":   request.Content,
+								"createdAt": time.Now().String(),
+						})
+
+						if err != nil {
+								fmt.Println(err)
+						}
+
+				}()
+
+				w.WriteHeader(http.StatusOK)
+				writer.Encode(&respose{
+						Message:   "Feed item was successfully added",
+						Status:    errorMsg,
+						Timestamp: time.Now().Unix(),
+				})
+		}
+}
+
+func authenticateUsers(client *pusher.Client) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+				// Handle CORS
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+				if r.Method == http.MethodOptions {
+						return
+				}
+
+				params, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+						w.WriteHeader(http.StatusBadRequest)
+						return
+				}
+
+				presenceData := pusher.MemberData{
+						UserID: "10",
+						UserInfo: map[string]string{
+								"random": "random",
+						},
+				}
+
+				response, err := client.AuthenticatePresenceChannel(params, presenceData)
+				if err != nil {
+						w.WriteHeader(http.StatusBadRequest)
+						return
+				}
+
+				w.Write(response)
+		}
 }
