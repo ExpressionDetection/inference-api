79891702c15e560c5947b124d8bd39d46ea9fb08
root <root@vagrant.vm>
2021-08-07 09:58:57 +0000

    fix boot

--- boot.sh
+++ boot.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Install dependencies
-cd /app/src && yarn install 
-
 if [ $RELOAD_APP_ON_FILE_CHANGE == "true" ]
   then
+    # Install dependencies
+    cd /app/src && yarn install 
     # Reload server whenever a file is saved
     cd /app/src && yarn run dev
   else
